bc81fb0966de31c3dad4c8a2b5c16a07706f1e96
Ghislain Hachey <1091769+ghachey@users.noreply.github.com>
2024-11-18 13:01:39 +0800

    feat: school supplies section

--- Pineapples.SurveyBuilder/Parts/SchoolSupplies.cs
+++ Pineapples.SurveyBuilder/Parts/SchoolSupplies.cs
@@ -19,15 +19,80 @@ namespace surveybuilder
 {
 	public class SchoolSupplies
 	{
+		// Import common table styles
+		PdfTableStylesheet ts = new PdfTableStylesheet();
 		public Document Build(KEMIS_PRI_Builder builder, Document document)
 		{
+			// TODO Move to reusable Cell stylesheets once all tables in survey complete the the cell styling is clear
+			var model = new Cell()
+				.SetHeight(20)
+				.SetVerticalAlignment(VerticalAlignment.MIDDLE)
+				.SetHorizontalAlignment(HorizontalAlignment.CENTER);
+
+			document.Add(builder.Heading_3("School Supplies for Students"));
+
 			document.Add(new Paragraph()
 				.Add(@"Did you receive all of the school supplies for students that were sent by MoE head office at the start of "
 				+ @"this year? You should have received enough for 1 set per student.Tick the box below that indicates the "
 				+ @"amount you received.")
-			);			
+			);
 
-			//document.Add(table);
+			var chk = new CheckBoxPickmaker();
+			chk.Names = new string[] { "None Received", "Some Received", "Half Received", "Most Received", "All Received" };
+			chk.Values = new int[] { 0,1, 2, 3, 4 };
+			chk.Types = new CheckBoxType[] {CheckBoxType.SQUARE, CheckBoxType.SQUARE
+					,CheckBoxType.SQUARE,CheckBoxType.SQUARE,CheckBoxType.CROSS };
+			chk.Tag = "Supplies.Student";
+			chk.Make(builder, document);
+
+			document.Add(builder.Heading_3("School Supplies for Teachers"));
+
+			document.Add(new Paragraph()
+				.Add(@"Did you receive all of the school supplies for Teachers that were sent by MoE head office at the start of "
+				+ @"this year? You should have received enough for 1 set per teacher.Tick the box below that indicates the "
+				+ @"amount you received.")
+			);
+
+			chk.Names = new string[] { "None Received", "Some Received", "Half Received", "Most Received", "All Received" };
+			chk.Values = new int[] { 0, 1, 2, 3, 4 };
+			chk.Types = new CheckBoxType[] {CheckBoxType.SQUARE, CheckBoxType.SQUARE
+					,CheckBoxType.SQUARE,CheckBoxType.SQUARE,CheckBoxType.CROSS };
+			chk.Tag = "Supplies.Teacher";
+			chk.Make(builder, document);
+
+			document.Add(builder.Heading_3("New Curriculum Materials"));
+
+			document.Add(new Paragraph()
+				.Add(@"Did you receive all of the school supplies for Teachers that were sent by MoE head office at the start of "
+				+ @"this year? You should have received enough for 1 set per teacher.Tick the box below that indicates the "
+				+ @"amount you received.")
+			);
+
+			Table table = new Table(UnitValue.CreatePercentArray(new float[] { 80, 10, 10 }))
+						.UseAllAvailableWidth();
+
+			PdfButtonFormField rgrp1 = new RadioFormFieldBuilder(builder.pdfDoc, "Supplies.Curriculum")
+				.CreateRadioGroup();
+			PdfButtonFormField rgrp2 = new RadioFormFieldBuilder(builder.pdfDoc, "Supplies.InUse")
+				.CreateRadioGroup();
+
+			table.AddRow(
+				ts.TableHeaderStyle(TextCell(model, ts.TableHeaderStyle(""))),
+				ts.TableHeaderStyle(TextCell(model, ts.TableHeaderStyle("Yes"))),
+				ts.TableHeaderStyle(TextCell(model, ts.TableHeaderStyle("No")))
+			);
+			table.AddRow(
+				TextCell(model, "Have you received new curriculum materials for Years 1 & 2?"),
+				YesCell(model, rgrp1),
+				NoCell(model, rgrp1)
+			);
+			table.AddRow(
+				TextCell(model, "Have you started using them in the classroom?"),
+				YesCell(model, rgrp2),
+				NoCell(model, rgrp2)
+			);
+
+			document.Add(table);
 
 			return document;
 		}
