911b28da5bc5696199704bb0df4fb58172476b46
softwords <github@softwords.com.au>
2024-12-17 09:43:57 +1100

    feat(teachers): wip of layout of teacher, expand to 80 teachers

--- Pineapples.SurveyBuilder/Parts/ExpectedStaff.cs
+++ Pineapples.SurveyBuilder/Parts/ExpectedStaff.cs
@@ -16,16 +16,36 @@ using static iText.IO.Codec.TiffWriter;
 using static surveybuilder.CellMakers;
 using surveybuilder.Utilities;
 using Org.BouncyCastle.Tls.Crypto.Impl;
+using iText.Kernel.Pdf.Action;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Annot;
 
 namespace surveybuilder
 {
 	public class ExpectedStaff
 	{
+		PdfStylesheet ss;
+		PdfStyle ThinHeader;
+		PdfStyle ThinHeaderLeft;
 		public Document Build(PdfBuilder builder, Document document)
 		{
 			Console.WriteLine("Part: Expected Staff");
 			// Import common table styles
+			ss = builder.stylesheet;
 			PdfTableStylesheet ts = new PdfTableStylesheet(builder.stylesheet);
+			ss.Add("colheaderthin", ThinHeader = new PdfStyle(ss["colheader"])
+			{
+				FontBold = false,
+				FontSize = 8,
+				Height = 12
+			});
+
+			// left text
+			ss.Add("tleft", ThinHeaderLeft = new PdfStyle(ss["colheaderthin"])
+			{
+				TextAlignment = TextAlignment.LEFT
+			}
+			);
 
 			document.Add(new Paragraph()
 				.Add(@"This list shows all the teachers recorded at your school in the last survey you submitted. "
@@ -45,28 +65,40 @@ namespace surveybuilder
 			var model15 = CellStyleFactory.FiveColumn;
 			var model21 = CellStyleFactory.TwoRowOneColumn;
 
-			document.Add(new Paragraph()
-				.Add(@"Sample of dropdown list populated with indirect /Opt")
-			);
-
 			var colwidths = new float[] { 40, 60, 60, 10, 10, 30, 10, 10 };
 			var subcolwidths = new float[] { 40, 60, 40, 60 };
 
-			const int MAX_TEACHERS = 10;
-			for (int i = 0; i <= MAX_TEACHERS; i++)
-			{
-				Table table = CellStyleFactory.DefaultTable(colwidths);
+			PdfAction onOnStaffChange = PdfAction.CreateJavaScript("actions.onOnStaffChange(event);");
 
-				table.AddHeaderRow(ts.TableHeaderStyle,
-					TextCell(model, "Payroll No")
-					, TextCell(model, "Family Name")
-					, TextCell(model, "Given Name")
+			const int MaxTeachers = 80;
+			for (int i = 0; i <= MaxTeachers; i++)
+			{
+				builder.NewPageIf(document, (i % 4 == 3));
+
+				Table table = CellStyleFactory.DefaultTable(colwidths);
+				table.AddHeaderRow(ThinHeader,
+					TextCell(model, "")
+					, TextCell(model, "")
+					, TextCell(model, "")
 					, TextCell(model12, "Gender")
-					, TextCell(model, "DoB")
+					, TextCell(model, "")
 					, TextCell(model12, "On Staff")
 				);
+				table.AddHeaderRow(ThinHeader,
+					TextCell(model, "Payroll No").Style(ThinHeaderLeft)
+					, TextCell(model, "Family Name").Style(ThinHeaderLeft)
+					, TextCell(model, "Given Name").Style(ThinHeaderLeft)
+
+					, TextCell(model, "M")
+					, TextCell(model, "F")
+					, TextCell(model, "DoB")
+					, TextCell(model, "Y")
+					, TextCell(model, "N")
+				);
+
 				var grpOnStaff = new RadioFormFieldBuilder(builder.pdfDoc, $"TL.{i:00}.OnStaff")
 					.CreateRadioGroup();
+
 				var grpGender = new RadioFormFieldBuilder(builder.pdfDoc, $"TL.{i:00}.Gender")
 					.CreateRadioGroup();
 				table.AddRow(
@@ -80,13 +112,14 @@ namespace surveybuilder
 					NoCell(model, grpOnStaff)
 				);
 
-				Table subtable = new Table(UnitValue.CreatePercentArray(subcolwidths))
-						.UseAllAvailableWidth();
+				Table subtable = CellStyleFactory.DefaultTable(subcolwidths)
+					.SetMarginBottom(0);
+
 				subtable.AddRow(
 					TextCell(model, "Citizenship"),
-					ComboCell(model, $"TL.{i:00}.Citizenship", builder.lookups.Opt("schoolTypes")),
+					ComboCell(model, $"TL.{i:00}.Citizenship", builder.lookups.Opt("nationalities")),
 					TextCell(model, "Home Island"),
-					ComboCell(model, $"TL.{i:00Ed}.HomeIsland", builder.lookups.Opt("schoolTypes"))
+					ComboCell(model, $"TL.{i:00}.HomeIsland", builder.lookups.Opt("islands"))
 					);
 
 				subtable.AddRow(
@@ -109,18 +142,25 @@ namespace surveybuilder
 					ComboCell(model, $"TL.{i:00}.Duties", builder.lookups.Opt("classLevels"))
 					);
 
-				table.AddRow(NestedTableCell(model13, subtable));
+				table.AddRow(ElementCell(model13, subtable).Style(ss["abstractcell"]));
 				document.Add(table);
 
+				foreach (var w in grpOnStaff.GetWidgets())
+				{
+					w.SetAdditionalAction(PdfName.U, onOnStaffChange);
+				}
 				// now we also have to create the hidden field for tID on every teacher instance
 				// like the Grid, if we create a form field and don;t place it on the page, it will
 				// exists somewhere in the ether not visible
 			}
 			var form = builder.GetPdfAcroForm();
-			for (int i = 0; i <= MAX_TEACHERS; i++)
+			for (int i = 0; i <= MaxTeachers; i++)
 			{
 				var txt = new TextFormFieldBuilder(builder.pdfDoc, $"TL.{i:00}.tID")
+				.SetWidgetRectangle(new Rectangle(10,10,10,10))
 				.CreateText();
+
+				txt.GetWidgets()[0].SetFlag(PdfAnnotation.HIDDEN);
 				form.AddField(txt);
 			}
 
