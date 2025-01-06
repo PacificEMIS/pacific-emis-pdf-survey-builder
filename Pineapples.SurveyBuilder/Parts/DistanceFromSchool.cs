7164228f76528fc218829dfac2ea48ec7a2fed1a
softwords <github@softwords.com.au>
2025-01-07 10:34:57 +1100

    feat(grids): DistanceFromSchool supports running row / column totals

--- Pineapples.SurveyBuilder/Parts/DistanceFromSchool.cs
+++ Pineapples.SurveyBuilder/Parts/DistanceFromSchool.cs
@@ -20,12 +20,14 @@ namespace surveybuilder
 {
 	public class DistanceFromSchool
 	{
+		PdfStylesheet ss;
 		public Document Build(PdfBuilder builder, Document document, LookupList distanceCodes)
 		{
 			Console.WriteLine("Part: Distance from School");
 
 			// Import common table styles
-			PdfTableStylesheet ts = new PdfTableStylesheet(builder.stylesheet);
+			ss = builder.stylesheet;
+			PdfTableStylesheet ts = new PdfTableStylesheet(ss);
 
 			// Cell layout/styling models
 			var model = CellStyleFactory.Default;
@@ -35,43 +37,50 @@ namespace surveybuilder
 			document.Add(new Paragraph(@"Record all pupils according to the distance they have to travel to reach the school "
 			+ "and their means of transport."));
 
-			Table table = new Table(UnitValue.CreatePercentArray(new float[] { 40, 20, 20, 20 }))
-						.UseAllAvailableWidth();
+			Table table = CellStyleFactory.DefaultTable(40, 20, 20, 20);
 
 			// first row of headings
-			table.AddRow(
-				ts.TableHeaderStyle(TextCell(model21, ts.TableHeaderStyle("Distance"))),
-				ts.TableHeaderStyle(TextCell(model13, ts.TableHeaderStyle("Number of Pupil")))
-			);
-			table.AddRow(
-				ts.TableHeaderStyle(TextCell(model, ts.TableHeaderStyle("On Foot"))),
-				ts.TableHeaderStyle(TextCell(model, ts.TableHeaderStyle("Transport"))),
-				ts.TableHeaderStyle(TextCell(model, ts.TableHeaderStyle("Total")))
+			table.AddRow(ts.TableHeaderStyle,
+				TextCell(model21, "Distance"),
+				TextCell(model13, "Number of Pupils")
 			);
 			// second row of headings
 
+			table.AddRow(ts.TableHeaderStyle,
+				TextCell(model, "On Foot"),
+				TextCell(model, "Transport"),
+				TextCell(model, "Total")
+			);
+			
 
 			// data rows
 			int i = 0;
 			foreach (var item in distanceCodes)
 			{
 				table.AddRow(
-					TextCell(model, ts.TableRowHeaderStyle(item.N)),
-					NumberCell(model, $"DT.D.{i:00}.00.All"),
-					NumberCell(model, $"DT.D.{i:00}.01.All"),
-					NumberCell(model, $"DT.T.{i:00}.T.All")
+					TextCell(model, item.N).Style(ts.TableHeaderStyle),
+					GridCell(model, $"DT.D.{i:00}.00.All"),
+					GridCell(model, $"DT.D.{i:00}.01.All"),
+					TotalCell(model, $"DT.T.{i:00}.T.All", "").Style(ss["tabletotal"])
 				);
 				i++;
 			}
 
-			// Totals
-			table.AddCell(ts.TableHeaderStyle(TextCell(model, ts.TableRowHeaderTotalStyle("Total"))));
+			// Totals row
+			table.AddRow(ts.TableHeaderStyle,
+				TextCell(model,"Total")
+			);
 
-			for (int j = 0; j < 3; j++)
+			for (int j = 0; j < 2; j++)
 			{
 				// TODO - Add support for read only NumberCell (See gendered grid)
-				table.AddCell(NumberCell(model, $"DT.T.T.{j:00}.All"));
+				table.AddRow(ss["tabletotal"],
+					TotalCell(model, $"DT.T.T.{j:00}.All")
+				);
 			}
+			table.AddRow(ss["tabletotal"],
+				TotalCell(model, $"DT.T.T.T.All")
+			);
 
 			document.Add(table);
 			return document;
