80b88f0c093e0edc4697ae404f9bad5d42eacc18
softwords <github@softwords.com.au>
2025-01-07 10:18:18 +1100

    feat(grids): Row / column headers now trak movement through grids; enhanced styling

--- Pineapples.SurveyBuilder/Components/GenderedGridmaker.cs
+++ Pineapples.SurveyBuilder/Components/GenderedGridmaker.cs
@@ -21,6 +21,8 @@ using System.Net;
 using iText.Commons.Actions.Data;
 using System.Runtime.Remoting.Contexts;
 using static surveybuilder.CellMakers;
+using System.Diagnostics;
+using surveybuilder.Utilities;
 
 namespace surveybuilder
 {
@@ -33,7 +35,7 @@ namespace surveybuilder
 
 		public Boolean RowTotals;
 		public Boolean ColumnTotals;
-		public Boolean IncludeFirstColumn;
+		public Boolean EditRowValue;
 
 		// Import common table/grid styles
 
@@ -43,73 +45,121 @@ namespace surveybuilder
 		{
 			RowTotals = true;
 			ColumnTotals = true;
-			IncludeFirstColumn = false;
+			EditRowValue = false;
 		}
 		public GenderedGridmaker(string name) { }
 
-		public Table Make(PdfBuilder builder)
+		public Table Make(PdfBuilder builder, Document document)
 		{
+		
 			PdfStylesheet ss = builder.stylesheet;
 			PdfTableStylesheet ts = new PdfTableStylesheet(ss);
 			// style names as used in the grid
+			//column header at the top of the grid - hold the colun labels
 			const string ColHeader = "colheader";
+			// second row of headers holds gender headers M and F
 			const string GenderHeader = "genderheader";
-			const string RowHeader = "rowheader";
+			// row header holds the row labels in left-most column of grid
+			const string OddRow = "oddrow";
+			const string EvenRow = "evenrow";
+			const string OddRowHeader = "oddrowheader";
+			const string EvenRowHeader = "evenrowheader";
+			const string OddRowTotal = "oddrowtotal";
+			const string EvenRowTotal = "evenrowtotal";
+			const string ColTotal = "coltotal";
+			const string ColTotalHeader = "coltotalheader";
+			const string GridTotal = "coltotal";
+			const string ColLabeller = "collabeller";
+			const string RowLabeller = "rowlabeller";
+
+			/*
+			 *		+------------------------------------------------------------------------------------------+
+			 *		+ (blank)   + colheader + colheader + colheader + colheader + colheader + rowtotalheader   +
+			 *		+------------------------------------------------------------------------------------------+
+			 *		+           + M  +  F  +  M  +  F  +  M  +  F  +  M  +  F  +  M  +  F  +  M  +  F  + Total +  (genderheader)
+			 *		+ rowheader + (evenrow / oddrow)                                       + rowtotal		   +
+			 *		+------------------------------------------------------------------------------------------+
+			 *		+ coltotalheader + coltotal											   + coltotal+ (blank)  +
+			 *		+ coltotalheader + coltotal											   + (blank) + gridtotal+
+			 * When EditRowValue is true, the top corner blanks are replaced by cells styled
+			 * collabeller and rowlabeller
+			 
+			 */
 			builder.pdfDoc.VerboseConsole($"GenderedGrid: {Tag}");
 
 			string color1 = "eaeaea";
 			string color2 = "cccccc";
 			string color3 = "a8a8a8";
 
+
 			//// cell models for table construction
 			// Row headings and data fields
 			Cell oddmodel = new Cell().SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color1));
+				.Style(ss["oddrow"]); ;
 
 			Cell oddmodel2 = new Cell(1, 2).SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color1));
+				.Style(ss["oddrow"]);
 
 			Cell evenmodel = new Cell().SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color2));
+				.Style(ss["evenrow"]);
 
 			var totalmodel = new Cell()
-				.SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color3));
+				.SetHeight(RowHeight);
+			//.SetBackgroundColor(Colors.WebColors.GetRGBColor(color3));
 
 			var totalmodel2 = new Cell(1, 2)
-				.SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color3));
+				.SetHeight(RowHeight);
+				//.SetBackgroundColor(Colors.WebColors.GetRGBColor(color3));
 
 			var colheadermodel = new Cell()
-				.SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color3));
+				.SetHeight(RowHeight);
 
 			var colheadermodel2 = new Cell(1, 2)
-				.SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color3));
+				.SetHeight(RowHeight);
+
 			var colheadermodel4 = new Cell(1, 4)
-				.SetHeight(RowHeight)
-				.SetBackgroundColor(Colors.WebColors.GetRGBColor(color3));
+				.SetHeight(RowHeight);
+				
 
-			// Create a table with )
-			Table table = new Table(UnitValue.CreatePercentArray(Columns.Count * 2 + 1 + (RowTotals ? 4 : 0) + (IncludeFirstColumn ? 1 : 0)))
-						  .UseAllAvailableWidth();
-
-			// Empty cell in the top-left corner
-			table.AddCell(new Cell().SetHeight(RowHeight));
-
-			// Column headings
-			if (IncludeFirstColumn)
+			// Create a table with a row heading array
+			int numValueCols = 1           // row label
+				+ Columns.Count * 2 + (RowTotals ? 4 : 0) ;
+			int[] widths = Enumerable.Repeat(10, numValueCols).ToArray();
+			if (EditRowValue)
 			{
-				table.AddCell(
-					TextCell(colheadermodel, "Age").Style(ss[ColHeader]));
+				//We make the first col wider to take the data entry. For fixed values there is 
+				// resizing to fit on the column
+				widths[0] = 30;
 			}
 
-			foreach (var kvp in Columns)
+			Table table = CellStyleFactory.DefaultTable(widths);
+
+			#region Top Row
+			
+			if (!EditRowValue)
+			{
+				// Empty cell in the top-left corner
+				table.AddRow(ss["abstractcell"],
+					TextCell(CellStyleFactory.TwoRowOneColumn, "")
+				);
+			}
+			
+			if (EditRowValue)
+			{
+				// labels for column labels and row label
+				table.AddRow(ss[ColLabeller],
+					TextCell(colheadermodel, "Age"));
+			}
+
+			for (int j = 0; j < Columns.Count; j++)
 			{
 				// rowspan is 2
-				table.AddCell(
-					TextCell(colheadermodel2, kvp.N).Style(ss[GenderHeader]));
+				table.AddRow(ss[ColHeader],
+					ValueCell(colheadermodel2, $"{Tag}.C.{j:00}.V", Columns[j].N,
+						StyleConfigurer(ss[ColHeader])
+					)
+				);
+
 
 			}
 			if (RowTotals)
@@ -117,52 +167,80 @@ namespace surveybuilder
 				table.AddCell(
 					TextCell(colheadermodel4, "Totals").Style(ss[ColHeader]));
 			}
+			#endregion
 
-			// gender headings row
-			// Empty cell in the top-left corner
-			table.AddCell(new Cell().SetHeight(RowHeight));
 
-			if (IncludeFirstColumn)
+			#region gender headings row
+
+			if (!EditRowValue)
 			{
-				table.AddCell(
-					TextCell(colheadermodel, ts.GridColHeader("Name of Pre-School")));
+				// Empty cell in the top-left corner created on row above
+				//table.AddCell(new Cell().SetHeight(RowHeight));
+			}
+			
+			if (EditRowValue)
+			{
+				table.AddRow(ss[RowLabeller],
+					TextCell(colheadermodel, "Name of preschool"));
 			}
 
 			for (int i = 0; i < Columns.Count; i++)
 			{
 				// rowspan is 1
-				table.AddRow(
-					TextCell(colheadermodel, "M").Style(ss[GenderHeader]),
-					TextCell(colheadermodel, "F").Style(ss[GenderHeader])
+				table.AddRow(ss[GenderHeader],
+					TextCell(colheadermodel, "M"),
+					TextCell(colheadermodel, "F")
 					);
 			}
 			if (RowTotals)
 			{
-				table.AddRow(
-					TextCell(colheadermodel, "M").Style(ss[GenderHeader]),
-					TextCell(colheadermodel, "F").Style(ss[GenderHeader]),
-					TextCell(colheadermodel2, "Totals (M+F)").Style(ss[GenderHeader])
+				table.AddRow(ss[GenderHeader],
+					TextCell(colheadermodel, "M"),
+					TextCell(colheadermodel, "F"),
+					TextCell(colheadermodel2, "Totals (M+F)")
 				);
 			}
 
+			#endregion
+
+			#region Data Rows
 
 			// data rows 
 			for (int i = 0; i < Rows.Count; i++)
 			{
 				// first the row header
 				var cellmodel = (i % 2 == 0) ? evenmodel : oddmodel;
-				table.AddCell(
-					TextCell(cellmodel, Rows[i].N.Nbsp()).Style(ss[RowHeader])
-				);
+				var rowStyle = (i % 2 == 0) ? ss[EvenRow]: ss[OddRow];
+				var headerStyle = (i % 2 == 0) ? ss[EvenRowHeader] : ss[OddRowHeader];
+				var totalStyle = (i % 2 == 0) ? ss[EvenRowTotal] : ss[OddRowTotal];
 
-				if (IncludeFirstColumn)
+				if (EditRowValue)
 				{
 					string name = $"{Tag}.R.{i:00}.V";
-					Paragraph pp = new Paragraph();
-					//pp.SetNextRenderer(new GridDataFieldCellRenderer(cellmodel, name));
-					table.AddCell(cellmodel.Clone(false).Add(pp));
+
+					table.AddRow(rowStyle,
+						InputCell(cellmodel,name)
+					);
 				}
 
+				if (!EditRowValue)
+
+				// if we add a paragraph here:
+				//TextCell(cellmodel, Rows[i].N.Nbsp())
+				// the table can automatically resize the column to fit
+				// but a form field does not force this resize
+				// we need to do it manually
+
+				{
+					float textwidth = headerStyle.Measure(Rows[i].N.Nbsp());
+
+					Debug.WriteLine($"{Rows[i].N.Nbsp()} {textwidth}");
+					table.AddRow(headerStyle,
+							ValueCell(cellmodel.Clone(false).SetMinWidth(textwidth), $"{Tag}.R.{i:00}.V", Rows[i].N.Nbsp(),
+							StyleConfigurer(headerStyle))
+					);
+
+				}
 				// Data fields for each row
 				for (int j = 0; j < Columns.Count; j++)
 				{
@@ -170,8 +248,10 @@ namespace surveybuilder
 					{
 						string name = $"{Tag}.D.{i:00}.{j:00}.{g}";
 						Paragraph pp = new Paragraph();
-						pp.SetNextRenderer(new GridDataFieldCellRenderer(cellmodel, name));
-						table.AddCell(cellmodel.Clone(false).Add(pp));
+						//pp.SetNextRenderer(new GridDataFieldCellRenderer(cellmodel, name));
+						table.AddRow(rowStyle,
+							GridCell(cellmodel, name)
+						);
 					}
 
 				}
@@ -182,24 +262,28 @@ namespace surveybuilder
 						string n = $"{Tag}.T.{i:00}.T.{g}";
 						Paragraph p = new Paragraph();
 						p.SetNextRenderer(new TotalFieldRenderer(totalmodel, n));
-						table.AddCell(totalmodel.Clone(false).Add(p));
+						table.AddRow(totalStyle, totalmodel.Clone(false).Add(p));
 					}
 					string name = $"{Tag}.T.{i:00}.T.T";
 					Paragraph pp = new Paragraph();
 					pp.SetNextRenderer(new TotalFieldRenderer(totalmodel2, name));
-					table.AddCell(totalmodel2.Clone(false).SetHeight(RowHeight).Add(pp));
+					table.AddRow(totalStyle, totalmodel2.Clone(false).SetHeight(RowHeight).Add(pp));
 				}
 
 			}
 
+			#endregion Data Rows
+
+			#region Column Gender Totals
+
 			if (ColumnTotals)
 			{
+
 				// gender totals
-				if (IncludeFirstColumn)
-				{
-					table.AddCell(new Cell().SetHeight(RowHeight));
-				}
-				table.AddCell(totalmodel.Clone(false).Add(ts.GridRowHeaderTotal("Totals")));
+				
+				table.AddRow(ss[ColTotalHeader],
+					TextCell(CellStyleFactory.TwoRowOneColumn,"Totals")
+				);
 
 				// Data fields for each row
 				for (int j = 0; j < Columns.Count; j++)
@@ -209,7 +293,7 @@ namespace surveybuilder
 						string name = $"{Tag}.T.T.{j:00}.{g}";
 						Paragraph pp = new Paragraph();
 						pp.SetNextRenderer(new TotalFieldRenderer(totalmodel, name));
-						table.AddCell(totalmodel.Clone(false).Add(pp));
+						table.AddRow(ss[ColTotal],totalmodel.Clone(false).Add(pp));
 					}
 				}
 				if (RowTotals)
@@ -219,51 +303,57 @@ namespace surveybuilder
 						string n = $"{Tag}.T.T.T.{g}";
 						Paragraph p = new Paragraph();
 						p.SetNextRenderer(new TotalFieldRenderer(totalmodel, n));
-						table.AddCell(totalmodel.Clone(false).Add(p));
+						table.AddRow(ss[ColTotal],totalmodel.Clone(false).Add(p));
 					}
 					Paragraph pp = new Paragraph();
-					table.AddCell(totalmodel2.Clone(false).Add(pp));
+					table.AddRow(ss["abstractcell"], 
+						TextCell(new Cell(1,2), "")
+					);
 				}
 			}
+
+			#endregion Column gender totals
+
+			#region column totals / grand total
 			if (ColumnTotals)
 			{
-				// column totals
-				if (IncludeFirstColumn)
-				{
-					table.AddCell(new Cell().SetHeight(RowHeight));
-				}
-				table.AddCell(totalmodel.Clone(false).Add(ts.GridRowHeaderTotal("Totals (M+F)")));
+				// from above 1 column
 
 				for (int j = 0; j < Columns.Count; j++)
 				{
 					string name = $"{Tag}.T.T.{j:00}.T";
 					Paragraph pp = new Paragraph();
 					pp.SetNextRenderer(new TotalFieldRenderer(totalmodel, name));
-					table.AddCell(totalmodel2.Clone(false).Add(pp));
+					table.AddRow(ss[ColTotal], totalmodel2.Clone(false).Add(pp));
 
 				}
 				if (RowTotals)
 				{
 					Paragraph p = new Paragraph();
-					table.AddCell(new Cell(1, 2).Add(p));
+					table.AddRow(ss["abstractcell"],
+						TextCell(CellStyleFactory.TwoColumn, "")
+					);
 
 					string name = $"{Tag}.T.T.T.T";
 					Paragraph pp = new Paragraph();
 					pp.SetNextRenderer(new TotalFieldRenderer(totalmodel, name));
-					table.AddCell(new Cell(1, 2).Add(pp));
+					table.AddRow(ss[GridTotal],
+						new Cell(1, 2).Add(pp));
 
 				}
 			}
+			#endregion column totals grand total
+
 
 			// export row and column info to xfdf
-			Columns.AsFields(builder.pdfDoc
-				, i => $"{Tag}.C.{i:00}.K", i => $"{Tag}.C.{i:00}.V");
-			Rows.AsFields(builder.pdfDoc
-				, i => $"{Tag}.R.{i:00}.K", i => $"{Tag}.R.{i:00}.V");
+			Columns.CodesAsFields(builder.pdfDoc
+				, i => $"{Tag}.C.{i:00}.K");
+			Rows.CodesAsFields(builder.pdfDoc
+				, i => $"{Tag}.R.{i:00}.K");
 
 			//// apply some formatting before returning the table
 			return table
-				.SetBorder(new Borders.SolidBorder(Colors.WebColors.GetRGBColor(color3), 1));
+			//	.SetBorder(new Borders.SolidBorder(Colors.WebColors.GetRGBColor(color3), 1));
 			;
 		}
 	}
